26a7441a3f4471d29ca80d21c4666b2c2cc1a8fb
Ertugrul Karademir <ekarademir@gmail.com>
2021-10-24 19:29:54 +0100

    Implemented client commit

--- src/client.rs
+++ src/client.rs
@@ -10,6 +10,7 @@ use chat::chat_client::ChatClient;
 use chat::{
     Member,
     JoinResponse,
+    NewChatMessage,
 };
 
 pub mod chat {
@@ -45,6 +46,29 @@ async fn main() {
                 )
                 .help("Join a server to obtain token")
             )
+            .subcommand(SubCommand::with_name("send")
+                .about("send a message to the server")
+                .arg(Arg::with_name("server")
+                    .short("s")
+                    .long("server")
+                    .value_name("SERVER")
+                    .default_value(DEFAULT_SERVER)
+                    .required(false)
+                )
+                .arg(Arg::with_name("token")
+                    .short("t")
+                    .long("token")
+                    .value_name("TOKEN")
+                    .required(true)
+                )
+                .arg(Arg::with_name("message")
+                    .short("m")
+                    .long("message")
+                    .value_name("MESSAGE")
+                    .required(true)
+                )
+                .help("Send a message to the server")
+            )
             .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("join") {
@@ -54,19 +78,41 @@ async fn main() {
         println!("Connecting {}", server_addr);
         if let Ok(mut client) = ChatClient::connect(server_addr).await {
             println!("Obtaining token");
-            if let Ok(join_result) = client.join(Request::new(Member {
+            match client.join(Request::new(Member {
                 username,
                 password
             })).await {
-                if join_result.get_ref().response == JoinResponse::Accepted as i32 {
-                    println!("Token: {}", join_result.get_ref().token);
-                }
-                if join_result.get_ref().response == JoinResponse::Denied as i32 {
-                    println!("Access denied");
-                }
-            } else {
-                println!("Can't connect");
-            };
-        }
+                Ok(join_result) => {
+                    if join_result.get_ref().response == JoinResponse::Accepted as i32 {
+                        println!("Token: {}", join_result.get_ref().token);
+                    }
+                    if join_result.get_ref().response == JoinResponse::Denied as i32 {
+                        println!("Access denied");
+                    }
+                },
+                Err(e) => println!("Can't obtain token {:?}", e)
+            }
+        } else {
+            println!("Can't connect");
+        };
+    }
+
+    if let Some(matches) = matches.subcommand_matches("send") {
+        let server_addr = matches.value_of("server").unwrap().to_string();
+        let token = matches.value_of("token").unwrap().parse().unwrap();
+        let message = matches.value_of("message").unwrap().to_string();
+        println!("Connecting {}", server_addr);
+        if let Ok(mut client) = ChatClient::connect(server_addr).await {
+            println!("Sending message");
+            match client.commit(Request::new(NewChatMessage {
+                token,
+                value: message
+            })).await {
+                Ok(commit_result) => println!("Last message time {:?}", commit_result.get_ref().time),
+                Err(e) => println!("Can't send message {:?}", e)
+            }
+        } else {
+            println!("Can't connect");
+        };
     }
 }
